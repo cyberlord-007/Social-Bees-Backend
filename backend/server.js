6d1a5692dc270fc672b9492f0d0d03be39041f11
cyberlord-007 <akashfzd2000@gmail.com>
2020-11-19 00:11:48 +0530

    created socket

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log('New user connected!');
+
+  socket.on('disconnect', () => {
+    console.log('user left ->');
+  });
+});
+
 app.use(router);
 
 server.listen(port, () => console.log(`server started on ${port}`));
